5701fdcefa3a7871850ef1949ae5f6e7e99855e8
Martin Casey <martinc140291@gmail.com>
2024-10-24 09:47:29 +1100

    first commit

---
+++ src/components/PageLinks.js
@@ -0,0 +1,14 @@
+import PageLink from './PageLink';
+import { pageLinks } from '../data';
+
+const PageLinks = ({ parentClass, itemClass }) => {
+  return (
+    <ul className={parentClass} id="nav-links">
+      {pageLinks.map((link) => {
+        return <PageLink key={link.id} link={link} itemClass={itemClass} />;
+      })}
+    </ul>
+  );
+};
+
+export default PageLinks;
